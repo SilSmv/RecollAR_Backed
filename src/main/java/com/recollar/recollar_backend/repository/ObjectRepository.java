c300cb534b9a6292935573ffe3691f145c0b7298
pjalvin <pomaalvin@gmail.com>
2021-10-26 01:13:54 -0400

    se agrego la logica para cambiar el estado de un objeto y se agrego la subida de imagenes pro objeto

--- src/main/java/com/recollar/recollar_backend/repository/ObjectRepository.java
+++ src/main/java/com/recollar/recollar_backend/repository/ObjectRepository.java
@@ -21,15 +21,28 @@ public interface ObjectRepository extends JpaRepository<ObjectModel,Integer> {
     @Query("update object o SET o.status = ?1, o.txHost = ?2, o.txUpdate = ?3 WHERE o.idObject = ?4")
     public void updateStatus(Integer status, String txHost, Date txUpdate, Integer idObject);
 
-    @Query(value = "select o.id_object idObject,o.name name, o.image image  from object o INNER JOIN collection c on c.id_collection=o.id_collection where o.id_collection = ?1 and o.status=1 and c.status=1 and c.id_collector=?2 order by o.id_object DESC", nativeQuery = true)
+    @Query(value = "select o.id_object idObject,o.name name, o.image image  from object o INNER JOIN collection c on c.id_collection=o.id_collection where o.id_collection = ?1 and o.status!=0 and c.status=1 and c.id_collector=?2 order by o.id_object DESC", nativeQuery = true)
     List<ObjectSimpleRequestInterface> findByCollectionId(Integer idCollection, Integer idCollector);
 
     @Query(value = "select * from object c where c.status = 1 and c.object_status = 2 or c.object_status = 3 order by c.id_collection DESC", nativeQuery = true)
     List<ObjectModel> getObjectAvailable();
 
-    @Query(value = "select * from object c where c.status = 1 and MATCH(c.name,c.description) AGAINST ( 'searchString') and c.object_status = 2 or c.object_status = 3 order by c.id_collection DESC", nativeQuery = true)
-    List<ObjectModel> getObjectSearch(@Param("searchString") String searchString);
+    @Query(value = "select * from object c where MATCH(c.name,c.description) AGAINST ( 'searchString' IN BOOLEAN MODE) and c.status = 2 or c.status = 3 order by c.id_object DESC", nativeQuery = true)
+    List<ObjectModel> getObjectsSearch(@Param("searchString") String searchString);
 
-    @Query(value = "select o.* from object o INNER JOIN collection c on c.id_collection=o.id_collection where o.id_object= ?1 and o.status=1 and c.status=1 and c.id_collector=?2", nativeQuery = true)
+    @Query(value = "select * from object c where  c.status = 2 or c.status = 3 order by c.id_object DESC", nativeQuery = true)
+    List<ObjectModel> getObjects();
+
+    @Query(value = "select o.* from object o INNER JOIN collection c on c.id_collection=o.id_collection where o.id_object= ?1 and o.status!=0 and c.status=1 and c.id_collector=?2", nativeQuery = true)
     ObjectModel getByIdWithVerification(@Param("idObject") Integer idObject,@Param("idCollector") Integer idCollector);
+    @Modifying
+    @Transactional
+    @Query("update object h SET h.txHost = ?1, h.txUpdate = ?2, h.image = ?3 WHERE h.idObject = ?4 and h.status != 0")
+    public void updateImage( String txHost, Date txUpdate, String image,Integer idObject);
+
+    @Modifying
+    @Transactional
+    @Query("update object h SET h.txHost = ?1, h.txUpdate = ?2, h.status = ?3 WHERE h.idObject = ?4 and h.status != 0")
+    public void updateObjectStatus( String txHost, Date txUpdate, Integer objectStatus,Integer idObject);
+
 }
