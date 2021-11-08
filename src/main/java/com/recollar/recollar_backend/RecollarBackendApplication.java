28adb94ec54184b120adf827a811ed52afdf282a
pjalvin <pomaalvin@gmail.com>
2021-11-08 09:17:09 -0400

    se modifico el pom.xml para poder realizar el deployment

--- src/main/java/com/recollar/recollar_backend/RecollarBackendApplication.java
+++ src/main/java/com/recollar/recollar_backend/RecollarBackendApplication.java
@@ -1,19 +1,11 @@
 package com.recollar.recollar_backend;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 @SpringBootApplication
 public class RecollarBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RecollarBackendApplication.class, args);
+        SpringApplication.run(RecollarBackendApplication.class, args); 
     }
 
 
