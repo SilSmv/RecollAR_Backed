8ce548443d53ee682a859fdd873e64fc1dd09fa9
pjalvin <pomaalvin@gmail.com>
2021-11-08 09:22:43 -0400

    se agrego la configuracion para el deployment

--- src/main/java/com/recollar/recollar_backend/RecollarBackendApplication.java
+++ src/main/java/com/recollar/recollar_backend/RecollarBackendApplication.java
@@ -1,12 +1,18 @@
 package com.recollar.recollar_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class RecollarBackendApplication {
+public class RecollarBackendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RecollarBackendApplication.class, args); 
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RecollarBackendApplication.class);
+    }
 
 }
